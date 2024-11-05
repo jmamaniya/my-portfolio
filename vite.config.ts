dde6d9f7901776ca6fc46df583de72e4e65cc8ab
jmamaniya <jmamaniya2022@gmail.com>
2024-11-05 10:06:14 -0500

    file updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
   base: "/my-portfolio/",
 });
+
+export default config;
